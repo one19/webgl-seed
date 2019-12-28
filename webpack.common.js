e81c1047e1b21d7176d4517dfebe91b8e9c62f6c
Drew Showalter <maelstroswim@gmail.com>
2019-12-28 14:51:06 -0600

    :recycle: include useful files like index.html & style.css in build.

--- webpack.common.js
+++ webpack.common.js
@@ -1,32 +1,39 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-	context: path.join(__dirname, 'src'),
-	entry: {
-		app: './main.ts'
-	},
-	plugins: [ new CleanWebpackPlugin() ],
-	output: {
-		filename: 'main.js',
-		path: path.resolve(__dirname, 'dist')
-	},
-	resolve: {
-		extensions: [ '.ts', '.tsx' ]
-	},
-	module: {
-		rules: [
-			{
-				test: /\.ts/,
-				exclude: /node_modules/,
-				loader: 'ts-loader',
-				options: {
-					transpileOnly: true,
-					compilerOptions: {
-						isolatedModules: true
-					}
-				}
-			}
-		]
-	}
+  mode: 'development',
+  context: path.join(__dirname, 'src'),
+  entry: {
+    app: './main.ts'
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyPlugin([
+      { from: '*.{css,html}', to: '[name].[ext]' }
+    ]),
+  ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: [ '.ts', '.tsx' ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts/,
+        exclude: /node_modules/,
+        loader: 'ts-loader',
+        options: {
+          transpileOnly: true,
+          compilerOptions: {
+            isolatedModules: true
+          }
+        }
+      }
+    ]
+  }
 };
