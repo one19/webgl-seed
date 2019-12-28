1b11f1f789528877cb8f779052679f810156a8c6
Drew Showalter <maelstroswim@gmail.com>
2019-12-28 14:52:14 -0600

    :poop: add some logging to our useless js files to find out what do.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import Renderer from './renderer';
 
-const node = document.getElementById('game');
+const node = document.getElementById('game') as HTMLCanvasElement;
 
 // 🏎️ Create rendering engine
 let renderer = new Renderer(node);
+console.log('rendnndndn', renderer, Object.keys(renderer), typeof renderer)
 
 // 🏁 Start your engine
-renderer.start();
+renderer.start(); // don't try to access nonexistent methods, kids
--- src/renderer.ts
+++ src/renderer.ts
@@ -1,12 +1,12 @@
 import * as glm from 'gl-matrix';
 
 // ⚪ GL Singleton
-var gl;
+var gl; // a linter worth a damn would throw here because unused
 
 export default class Renderer
 {
-	constructor(node: HTMLCanvasElement)
-	{
+  constructor(node: HTMLCanvasElement)
+  {
 
-	}
+  }
 }
